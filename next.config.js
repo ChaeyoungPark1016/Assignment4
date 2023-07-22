b4fa02e5b53f28e6e7a156bab4493ee7385c0611
Chloe Chaeyoung Park <96512433+ChaeyoungPark1016@users.noreply.github.com>
2023-07-21 21:33:57 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-const path = require('path');
-
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname);
-    return config;
-  },
-};
+}
+
+module.exports = nextConfig

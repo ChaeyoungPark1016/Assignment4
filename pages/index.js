9a404da1e0e0622de75138df270e7f726df75ed8
ChaeyoungPark1016 <cypark1@myseneca.ca>
2023-07-21 20:00:08 -0400

    A5

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 /*********************************************************************************
- *  WEB422 – Assignment 4
+ *  WEB422 – Assignment 5
  *  I declare that this assignment is my own work in accordance with Seneca Academic Policy.
  *  No part of this assignment has been copied manually or electronically from any other source
  *  (including web sites) or distributed to other students.
  *
- *  Name: Chaeyoung Park  Student ID: 011784154    Date: Jul 5, 2023
+ *  Name: Chaeyoung Park  Student ID: 011784154    Date: July 21, 2023
  *  Deploy Link:
  ********************************************************************************/
 
